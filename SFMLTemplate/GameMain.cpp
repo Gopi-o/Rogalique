e5ef4ab39dc93fd7d8870691857d93add400a64e
Gopi-o <Aleshi.kriv@Gmail.com>
2025-06-06 01:01:49 +0400

    2 stage for 2D Engine

--- SFMLTemplate/GameMain.cpp
+++ SFMLTemplate/GameMain.cpp
@@ -3,11 +3,30 @@
 
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
+#include "Engine.h"
+//#include <Windows.h>
+//#include <iostream>
 
 const std::string RESOURCES_PATH = "Resources/";
 
 int main()
 {
+	//if (AllocConsole())
+	//{
+	//	FILE* fp;
+	//	freopen_s(&fp, "CONOUT$", "w", stdout);
+	//	freopen_s(&fp, "CONOUT$", "w", stderr);
+	//}
+	//else
+	//{
+	//	std::cerr << "Не удалось выделить консоль." << std::endl;
+	//}
+
+	/*XYZEngine engine;*/
+	//XYZEngine::Engine engine;
+	////engine.Initialize();
+	//engine.Run();
+
 	sf::RenderWindow window(sf::VideoMode(330, 400), "SFML works!");
 
 	sf::Texture logo;
